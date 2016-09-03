86df475119d07cba766feac45d397ea72c6af622
kobe <m18530089913@163.com>
2016-09-03 13:31:15 +0800

    new file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+require(['math'], function (math){
+    alert(math.add(1,1));
+});
+
---
+++ js/math.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+define(function (){
+    var add = function (x,y){
+        return x+y;
+    };
+    return {
+        add: add
+    };
+});
